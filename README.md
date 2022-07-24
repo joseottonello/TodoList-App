b3c58887a9755ef24853e0d387145cfa982490ef
José Ottonello <dev.ottonellojose@gmail.com>
2022-07-24 14:08:57 -0300

    add gif

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# to-do list
-- Tecnologias usadas: HTML, CSS y JavaScript
+# Notes
+This project was created with HTML, CSS and JavaScript. Sass was also incorporated for CSS support.
+In this application you can create pending tasks to be carried out as well as cross off the tasks already carried out.
+
+## Contents
+This project has two branches:
+"main" branch: where all its content is located.
+"gh-pages" branch: where your "deploy" is located
+
+## Demo
+If you want to see the demo of this project deployed you can visit [Project demo](joseottonello.github.io/app-todolist//)
+
+## How to clone
+To clone this repository you need:
+* Copy the repository link
+* go to your code editor
+* open terminal and type:
+```bash
+git clone + link
+```
+
+## Preview
++++++++++++++++++++++++
+![](./assets/gif.gif)
+
+### Notes
+If you want to learn all about Sass I recommend you visit the site [Sass-long.com](https://sass-lang.com//)
+
+If you want to learn everything about Markdown I recommend that you visit the site [markdown.es](https://markdown.es/sintaxis-markdown/)
---
+++ js/index.js
@@ -0,0 +1,46 @@
+const 
+dateNumber = document.getElementById('dateNumber'),
+dateMonth = document.getElementById('dateMonth'),
+dateYear = document.getElementById('dateYear'),
+dateText = document.getElementById('dateText'),
+tasksContainer = document.getElementById('tasksContainer');
+
+
+const setDate = () => {
+    const date = new Date();
+    dateNumber.textContent = date.toLocaleString('en', { day: 'numeric' });
+    dateText.textContent = date.toLocaleString('en', { weekday: 'long' });
+    dateMonth.textContent = date.toLocaleString('en', { month: 'long' });
+    dateYear.textContent = date.toLocaleString('en', { year: 'numeric' });
+};
+
+const addNewTask = event => {
+    event.preventDefault();
+    const { value } = event.target.taskText;
+    if(!value) return;
+    const task = document.createElement('div');
+    task.classList.add('task', 'roundBorder');
+    task.addEventListener('click', changeTaskState)
+    task.textContent = value;
+    tasksContainer.prepend(task);
+    event.target.reset();
+};
+
+const changeTaskState = event => {
+    event.target.classList.toggle('done');
+};
+
+const order = () => {
+    const done = [];
+    const toDo = [];
+    tasksContainer.childNodes.forEach( el => {
+        el.classList.contains('done') ? done.push(el) : toDo.push(el)
+    })
+    return [...toDo, ...done];
+}
+
+const renderOrderedTasks = () => {
+    order().forEach(el => tasksContainer.appendChild(el))
+}
+
+setDate();
